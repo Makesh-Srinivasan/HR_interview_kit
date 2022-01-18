a7ef3b7b06701822a32a8f8ac26987d252614571
Makesh Srinivasan <himakesh2001@gmail.com>
2022-01-18 20:08:59 +0530

    added 2 solutions from week 3

---
+++ Week 3/Sales_by_match.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        // Write your code here
+        HashMap<Integer,Integer> dict=new HashMap<Integer,Integer>();
+        for(int i = 0; i < n; i++){
+            if(dict.containsKey(ar.get(i))){
+                int x = ar.get(i);
+                int old = dict.get(x);
+                dict.replace(x, old, old + 1);
+            } else {
+                dict.put(ar.get(i), 1);
+            }
+        }
+        int count = 0;
+        for(Integer e: dict.values()){
+            if(e%2 == 0){
+                count += e/2;
+            } else {
+                if(e != 1){
+                    count += e/2;
+                }
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
